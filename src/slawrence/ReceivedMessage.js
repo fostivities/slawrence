db5b947d47dad5144f918a4e3f2a29c1d696ba8c
Jordan <jordan.foster11@gmail.com>
2018-02-01 21:10:44 -0800

    save

--- src/slawrence/ReceivedMessage.js
+++ src/slawrence/ReceivedMessage.js
@@ -7,9 +7,11 @@ class ReceivedMessage {
         this.text = cleanAndValidateText(message.text);
     }
 
+    const commands = ['--help', 'set', 'take', 'won', 'cancel']
+
     cleanAndValidateText (text) {
         text = text.toLowerCase();
-        
+
         if (checkForSB(text)) {
             this.isValid = true;
             return removeSBName(text);
@@ -27,6 +29,27 @@ class ReceivedMessage {
         return text.indexOf(allowedNames[0]) > -1
             ? text.replace(allowedNames[0], '')
             : cleanText = text.replace(allowedNames[1], '');
+    }
+
+    setCommand = (text) => {
+        const commands = ['--help', 'set', 'take', 'won', 'cancel'];
+        let lowestIndex = null;
+        let firstCommand = '';
+        let response = '';
+
+        for (let command of commands) {
+            let commandIndex = text.indexOf(command);
+
+            if (commandIndex > -1) {
+                if (lowestIndex === null) {
+                    lowestIndex = commandIndex;
+                    firstCommand = command;
+                } else if (commandIndex <= lowestIndex) {
+                    lowestIndex = commandIndex;
+                    firstCommand = command;
+                }
+            }
+        }
     }
 }
 
