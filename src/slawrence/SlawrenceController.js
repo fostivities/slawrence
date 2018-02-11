a02f509c709f15b75a2694c8b6db8562395a883c
Jordan <jordan.foster11@gmail.com>
2018-02-11 14:58:02 -0800

    setting up to handle two different groups

--- src/slawrence/SlawrenceController.js
+++ src/slawrence/SlawrenceController.js
@@ -10,14 +10,16 @@ const commandController = require('./components/commandController');
 const allowedNames = ['@sb ', '@slawrence '];
 const groupMeUrl = 'https://api.groupme.com/v3/bots/post';
 const response = {
-    "bot_id": "4091fd6b5183549c40fd901abc",
-    "text": ""
+    'bot_id': '',
+    'text': ''
 };
 
 router.use(bodyParser.urlencoded({ extended: true }));
 router.use(bodyParser.json());
 
 const handleGroupMePost = (req, res) => {
+    response.bot_id = req.body.group_id === 35018764 ? '4091fd6b5183549c40fd901abc' : '';
+
     if (req.body.sender_type !== 'bot' && req.body.text && req.body.text.indexOf('@sb ') > -1) {
         let message = new Message(req.body);
 
@@ -34,8 +36,6 @@ const handleGroupMePost = (req, res) => {
                 })
         }
     }
-
-    // res.status(200).send();
 }
 
 const respond = (res) => {
@@ -44,9 +44,9 @@ const respond = (res) => {
     } else {
         superAgent.post(groupMeUrl)
             .send(response)
-            // .then(() => {
-            //     res.status(200).send();
-            // })
+            .then(() => {
+                res.status(200).send();
+            })
             .catch((err) => {
                 res.status(200).end('There was an error in posting. Error: ' + err);
             });
