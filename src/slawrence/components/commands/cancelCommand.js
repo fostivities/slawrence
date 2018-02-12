8eb1a2e65ff50eea1aac8c1be17afb40881d0239
Jordan <jordan.foster11@gmail.com>
2018-02-11 16:12:38 -0800

    added cancel command

--- src/slawrence/components/commands/cancelCommand.js
+++ src/slawrence/components/commands/cancelCommand.js
@@ -1,5 +1,22 @@
-const cancelCommand = () => {
+const superAgent = require('superagent');
+const errorComannd = require('./errorCommand');
 
+const cancelCommand = (message) => {
+    return new Promise((resolve, reject) => {
+
+        if (message.betAmountOrID) {
+            superAgent.delete('https://slawrence.herokuapp.com/bets/' + message.betAmountOrID)
+                .send()
+                .then(() => {
+                    resolve('Bet ' + message.betAmountOrID + ' cancelled and deleted from DB.');
+                })
+                .catch((err) => {
+                    resolve('There was an error in deleting this bet.');
+                });
+        } else {
+            resolve(errorCommand(2));
+        }
+    });
 }
 
 module.exports = cancelCommand;
